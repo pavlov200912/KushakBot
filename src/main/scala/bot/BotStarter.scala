6f26b3a5cbe5e80b20dbfa70a380ddcb6f28da0b
pavlov200912 <pavlov200912@gmail.com>
2020-03-02 17:56:48 +0300

    lab3 done

--- src/main/scala/bot/BotStarter.scala
+++ src/main/scala/bot/BotStarter.scala
@@ -1,5 +1,6 @@
 package bot
 
+import bot.BotStarter.Service
 import cats.instances.future._
 import cats.syntax.functor._
 import com.bot4s.telegram.api.RequestHandler
@@ -18,18 +19,104 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.io.Source
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.json4s._
+import org.json4s.native.Serialization
 
 import scala.util.{Failure, Success}
+
+class BotStarter(override val client: RequestHandler[Future], val service: Service) extends TelegramBot
+  with Polling
+  with Commands[Future]{
+
+  val messages = scala.collection.mutable.Map[String, mutable.MutableList[(String, String)]]()
+    .withDefaultValue(mutable.MutableList())
+  val registeredUsers = mutable.Set[User]()
+  onCommand("/start") { implicit msg =>
+    //msg.chat.id
+    //msg.from
+    msg.from match {
+      case None => reply("Error").void
+      case Some(x) => {
+        registeredUsers += x
+        reply(s"hi\nyour id: ${x.id}").void
+      }
+    }
+  }
+
+  onCommand("/users") {implicit msg =>
+    var answer = ""
+    registeredUsers.foreach {
+      it =>
+        answer += s"${it.firstName} ${it.lastName.getOrElse("")}, id: ${it.id}\n"
+    }
+    reply(answer).void
+  }
+
+  onCommand("/send") { implicit msg =>
+    msg.from match {
+      case None => reply("ERROR").void
+      case (Some (x)) => withArgs { args =>
+        val id = args.head
+        if (!messages.contains(id)) {
+          messages(args.seq.head) = mutable.MutableList()
+        }
+        messages(args.head) += (x.id.toString -> args.tail.foldLeft("")((acc, word) => acc + word + " "))
+        reply("Message was sent").void
+      }
+    }
+  }
+
+  onCommand("/check") { implicit msg =>
+    var answer = ""
+    msg.from match {
+      case None => reply("ERROR").void
+      case (Some(x)) => messages(x.id.toString).foreach { pair =>
+        answer += s"Message: ${pair._2} from: ${pair._1} \n"
+      }
+    }
+    reply(answer).void
+  }
+
+  def unwrapName(option: Option[String]): String = option match {
+    case None => ""
+    case Some(x) => x
+  }
+
+  onCommand("/iam") {implicit msg =>
+    msg.from match {
+      case None => reply("I don't know u((").void
+      case Some(x) => reply(s"Hello, ${x.firstName} ${unwrapName(x.lastName)}").void
+    }
+  }
+
+  onCommand("/cats") {implicit msg =>
+    service.getCat().flatMap(reply(_)).void
+  }
+
+}
 
 
 object BotStarter {
 
-  implicit val serialization = org.json4s.native.Serialization
+  implicit val serialization: Serialization.type = org.json4s.native.Serialization
 
   case class Response(data: List[Data])
-  case class Data(title: String)
+  case class Data(link: String)
 
-  // data[].images[].link
+  class Service(implicit val backend: SttpBackend[Future, Nothing]) {
+    implicit val ec: ExecutionContext = ExecutionContext.global
+    val request: RequestT[Id, Response, Nothing] = sttp
+      .header("Authorization", "Client-ID 20ff243e5fe83b7")
+      .get(uri"https://api.imgur.com/3/gallery/search?q=cats")
+      .response(asJson[Response])
+
+    def getCat(): Future[String] = {
+      backend.send(request).map {
+            // TODO: random item
+        response => scala.util.Random.shuffle(response.unsafeBody.data).head.link
+      }
+    }
+
+  }
 
   def main(args: Array[String]): Unit = {
     // Рулит потоками
@@ -38,16 +125,15 @@ object BotStarter {
       SttpBackendOptions.Default.socksProxy("ps8yglk.ddns.net", 11999)
     )
 
-    val request: RequestT[Id, Response, Nothing] = sttp
-      .header("Authorization", "Client-ID 20ff243e5fe83b7")
-      .get(uri"https://api.imgur.com/3/gallery/search?q=cats")
-      .response(asJson[Response])
+    val filename = "token.txt"
+    val fileSource = Source.fromFile(filename)
+    val token =  fileSource.mkString
+    fileSource.close()
 
-    val res: Future[Unit] = backend.send(request).map {
-      response => response.unsafeBody
-    }.map(println)
+    val service: Service = new Service()
+    val bot = new BotStarter(new FutureSttpClient(token), service)
 
-    Await.ready(res, Duration.Inf)
+    Await.result(bot.run(), Duration.Inf)
   }
 
 }
